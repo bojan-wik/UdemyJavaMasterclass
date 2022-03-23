d8fa63cc55326b84fec4f97dc059955d3de54415
bojan-wik <bojan.wik@protonmail.com>
2022-03-23 17:36:45 +0100

    init commit

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,39 @@
+package inheritance;
+
+// Animal - parent class
+public class Animal {
+
+    // fields (states), które są obecne u wszystkich zwierząt
+    private String name;
+    private boolean brain;
+    private int weight;
+
+    // constructor
+    public Animal(String name, boolean brain, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.weight = weight;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public boolean getBrain() {
+        return brain;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    // methods (behaviours) które są obecne u wszystkich zwierząt
+    public void move() {
+        System.out.println("Animal.move() called");
+    }
+
+    public void eat() {
+        System.out.println("Animal.eat() called");
+    }
+}
---
+++ src/inheritance/Main.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("Animal-1", true, 25);
+        //System.out.println(animal1.getWeight());
+
+        Dog dog1 = new Dog("Mops", true, 15, 4, true, "nice fur");
+        //System.out.println(dog1.getName());
+        dog1.move();
+        dog1.eat();
+    }
+}
