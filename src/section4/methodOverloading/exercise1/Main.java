c85f07af92f1e78d914199ac9b474db0caf3abc9
bojan-wik <bojan.wik@protonmail.com>
2022-04-27 10:07:27 +0200

    section4.methodOverloading.exercise1

--- src/section4/methodOverloading/exercise1/Main.java
+++ src/section4/methodOverloading/exercise1/Main.java
@@ -4,21 +4,27 @@ public class Main {
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
         if (feet >= 0 && (inches >= 0 && inches <= 12)) {
-            return ((feet * 12) + inches) * 2.54;
+            double centimeters = ((feet * 12) + inches) * 2.54;
+            System.out.printf("%.2f feet, %.2f inches = %.2f cm", feet, inches, centimeters).println();
+            return centimeters;
         } else {
+            System.out.println("Invalid feet or inches parameters");
             return -1;
         }
     }
 
     public static double calcFeetAndInchesToCentimeters(double inches) {
         if (inches >= 0) {
-            //code
+            double feet = (int)inches / 12;
+            double remainingInches = (int)inches % 12;
+            System.out.printf("%.2f inches = %.2f feet, %.2f inches", inches, feet, remainingInches).println();
+            return calcFeetAndInchesToCentimeters(feet, remainingInches);
         } else {
             return -1;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(calcFeetAndInchesToCentimeters(6, 10));
+        calcFeetAndInchesToCentimeters(100);
     }
 }
