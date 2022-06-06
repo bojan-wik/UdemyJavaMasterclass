e4595689bfc6d838244745115a57e305be014972
bojan-wik <bojan.wik@protonmail.com>
2022-06-06 15:38:08 +0200

    section8/linkedList

--- src/section8/linkedList/Demo.java
+++ src/section8/linkedList/Demo.java
@@ -2,16 +2,18 @@ package section8.linkedList;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
 
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<String>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
+        placesToVisit.add("Adelaide");
         placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
+        placesToVisit.add("Melbourne");
 
+        printList(placesToVisit);
+        addInOrderAZ(placesToVisit, "Cyce");
         printList(placesToVisit);
     }
 
@@ -21,5 +23,26 @@ public class Demo {
             System.out.println(iterator.next());
         }
         System.out.println("===============================");
+    }
+
+    private static boolean addInOrderAZ(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext()) {
+            int comparison = listIterator.next().compareTo(newCity);
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.printf("%s is already on the list", newCity);
+                return false;
+            } else if (comparison > 0) {
+                // not equal, add before
+                listIterator.previous();
+                listIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // not equal, move on next city
+            }
+        }
+        listIterator.add(newCity);
+        return true;
     }
 }
