6dd2a9b076b11abb8d06f6febd67e9cc93e10ee0
bojan-wik <bojan.wik@protonmail.com>
2022-07-21 15:13:12 +0200

    section10/javaGenerics/teamAndPlayerExample/

--- src/section10/javaGenerics/teamAndPlayerExample/Main.java
+++ src/section10/javaGenerics/teamAndPlayerExample/Main.java
@@ -3,15 +3,28 @@ package section10.javaGenerics.teamAndPlayerExample;
 public class Main {
 
     public static void main(String[] args) {
-        BaseballPlayer baseballPlayer1 = new BaseballPlayer("Joe");
         FootballPlayer footballPlayer1 = new FootballPlayer("Vic");
+        BaseballPlayer baseballPlayer1 = new BaseballPlayer("Joe");
         SoccerPlayer soccerPlayer1 = new SoccerPlayer("Pepe");
 
-        Team teamA = new Team("TeamA");
-        teamA.addPlayer(baseballPlayer1);
-        teamA.addPlayer(footballPlayer1);
-        teamA.addPlayer(soccerPlayer1);
+        Team<FootballPlayer> footballTeam = new Team<>("Football Team");
+        footballTeam.addPlayer(footballPlayer1);
+        // to da errory, jako że ta instancja do klasy 'Team' przyjmuje parametr typu 'FootballPlayer'
+//        footballTeam.addPlayer(baseballPlayer1);
+//        footballTeam.addPlayer(soccerPlayer1);
 
-        System.out.println(teamA.getNumOfPlayers());
+        System.out.println(footballTeam.getNumOfPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Baseball Team");
+        baseballTeam.addPlayer(baseballPlayer1);
+
+        System.out.println(baseballTeam.getNumOfPlayers());
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("broken team");
+        brokenTeam.addPlayer(soccerPlayer1);
+
+        System.out.println(brokenTeam.getNumOfPlayers());
+
+//
     }
 }
--- src/section10/javaGenerics/teamAndPlayerExample/Team.java
+++ src/section10/javaGenerics/teamAndPlayerExample/Team.java
@@ -2,7 +2,7 @@ package section10.javaGenerics.teamAndPlayerExample;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> {
 
     private String name;
     int played = 0;
@@ -10,7 +10,7 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -24,26 +24,31 @@ public class Team {
         return members.size();
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println("Player " + player.getName() +  " is already on the team " + this.getName());
+            System.out.println("Player " + (player.getName() +  " is already on the team " + this.getName()));
             return false;
         }
         members.add(player);
-        System.out.println("Adding player " + player.getName() + " to the team " + this.getName());
+        System.out.println("Adding player " + (player.getName() + " to the team " + this.getName()));
         return true;
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+        String message;
         played += 1;
         if (ourScore > theirScore) {
             won += 1;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied += 1;
+            message = " drew with ";
         } else {
             lost += 1;
+            message = " lost to ";
         }
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
